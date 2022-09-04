794430bea4c6b02ddc3af00902c7f0f0174a454b
RunyShark <92100746+RunyShark@users.noreply.github.com>
2022-09-03 20:04:34 -0500

    prisma-docker

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
+const bootstrap = async () => {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.useGlobalPipes(
@@ -12,5 +12,5 @@ async function bootstrap() {
     })
   );
   await app.listen(3000);
-}
+};
 bootstrap();
