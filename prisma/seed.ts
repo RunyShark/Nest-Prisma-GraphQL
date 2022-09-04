42d2a83b556051310d744d7069a5f1f5d47c7b47
RunyShark <92100746+RunyShark@users.noreply.github.com>
2022-09-03 23:30:25 -0500

    parseDatajsonFS

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,14 +1,25 @@
+import { readdirSync, readFileSync } from 'fs';
+import { join } from 'path';
 import { PrismaClient } from '.prisma/client';
 
 const prisma = new PrismaClient();
 
+const dataPath = join(__dirname, './', 'data');
+const filesNames = readdirSync(dataPath);
+
+const files = filesNames.map((files: string) => ({
+  name: files.split('.')[0],
+  data: JSON.parse(readFileSync(`${dataPath}/${files}`, 'utf-8')),
+}));
+
 const main = async () => {
+  console.log(files);
   //await prisma.category.deleteMany();
-  await prisma.category.create({
-    data: {
-      name: 'Explame',
-    },
-  });
+  //   await prisma.category.create({
+  //     data: {
+  //       name: 'Explame',ls
+  //     },
+  //   })
 };
 
 main()
