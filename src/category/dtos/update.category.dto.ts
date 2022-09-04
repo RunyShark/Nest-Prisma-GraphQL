e6914b8efa7994cd640796d0f060bb53cb2d9a71
RunyShark <92100746+RunyShark@users.noreply.github.com>
2022-09-04 15:14:16 -0500

    updateCategory

--- src/category/dtos/update.category.dto.ts
+++ src/category/dtos/update.category.dto.ts
@@ -1,7 +1,7 @@
-import { Field, PartialType } from '@nestjs/graphql';
+import { Field, InputType, PartialType } from '@nestjs/graphql';
 import { IsNotEmpty } from 'class-validator';
 import { CategoryDTO } from './category.dto';
-
+@InputType()
 export class UpdateCategoryDTO extends PartialType<CategoryDTO>(CategoryDTO) {
   @IsNotEmpty()
   @Field({ nullable: true })
